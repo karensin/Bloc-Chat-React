d51c1385fb419789bf97b7bb120e9214a3ef4920
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2018-11-30 15:11:22 -0800

    chat-room-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList.js';
 
+  var config = {
+    apiKey: "AIzaSyDxQe-GbXvFlfi8KqMe3jCMxXbi2y14vso",
+    authDomain: "chatty-1b82d.firebaseapp.com",
+    databaseURL: "https://chatty-1b82d.firebaseio.com",
+    projectId: "chatty-1b82d",
+    storageBucket: "chatty-1b82d.appspot.com",
+    messagingSenderId: "762771235835"
+  };
+  firebase.initializeApp(config);
+ 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <h1> Bloc chat</h1>
+        <RoomList firebase={firebase} />
       </div>
+
+
     );
   }
 }
