49f49a413ae42a7d134189fe82138b3211a9e19a
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2018-12-11 20:56:08 -0800

    message list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import './App.css';
 import RoomList from './components/RoomList.js';
+import MessageList from './components/MessageList.js';
 
   var config = {
     apiKey: "AIzaSyDxQe-GbXvFlfi8KqMe3jCMxXbi2y14vso",
@@ -14,16 +15,41 @@ import RoomList from './components/RoomList.js';
   firebase.initializeApp(config);
 
 class App extends Component {
-
+  constructor(props){
+    super(props);
+    this.state={
+      activeRoom:'',
+      user:''
+    };
+    this.setActiveRoom=this.setActiveRoom.bind(this);
+    this.setUser=this.setUser.bind(this);
+}
+    setActiveRoom(room){
+      this.setState({
+        activeRoom: room
+      });
+    }
+    setUser(user){
+      this.setState({user:user});
+    }
   render() {
+    const listMessages = this.state.activeRoom;
+
     return (
-      <div className="App">
 
-        <h1> Bloc chat</h1>
-        <RoomList firebase={firebase} />
-      </div>
+     <div className="App">
 
-
+       <h1>Chatty</h1>
+       <RoomList
+       firebase={firebase}
+        setActiveRoom={this.setActiveRoom}
+       />
+       <aside>
+       { listMessages ?
+         <MessageList firebase = {firebase} activeRoom = {this.state.activeRoom.key}/>
+         : null }
+         </aside>
+       </div>
     );
   }
 }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 
+
 class RoomList extends Component {
   constructor(props) {
     super(props);
@@ -9,63 +10,69 @@ class RoomList extends Component {
       newRoom:'',
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+     this.setActiveRoom = this.props.setActiveRoom.bind(this);
   }
 
-  componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
-       const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) })
-     });
-   }
 
-
-   handleChange(e){
-      this.setState({name: e.target.value});
-   }
-
-   componentWillUnmount(){
-     this.roomsRef.off();
-   }
-
-   createRoom(e, newRoom){
-
-     if (newRoom) {
-       e.preventDefault();
-       this.roomsRef.push({
-          name: newRoom
-        });
-         this.setState({newRoom:''});
+    componentDidMount() {
+      this.roomsRef.on('child_added', snapshot => {
+        const room = snapshot.val();
+        room.key = snapshot.key;
+        this.setState({ rooms: this.state.rooms.concat(room)});
+      });
+    }
+    createRoom(){
+      if (!this.state.newRoom.trim()!=='')
+        {
+          this.roomsRef.push(
+        {
+          name: this.state.newRoom,
+        }
+      );
+      this.setState({newRoom:''})
+    }
+      else {
+          alert('Enter a room name');
+          this.setState({
+            newRoom:'',
+          });
       }
     }
 
-       handleRoomInput(e){
-         this.setState({newRoom:e.target.value});
-       }
-   render () {
-     return (
-         <div>
-          <ul className='roomList'>
-           {
-             this.state.rooms.map( (room) =>
-               <li key={room.key}>{room.name}</li>
-             )
-           }
+    handleUserInput(e){
+  this.setState({
+    newRoom: e.target.value});
+}
 
-           <form onSubmit={(e)=> this.createRoom(e,this.state.newRoom)}>
-              <label>
-                New room:
-                <input type="text" value={this.state.newRoom} onChange={e => this.handleRoomInput(e)}/>
-              </label>
-            <input type= "Submit" value="Create Room"/>
-          </form>
-
-
-
-        </ul>
-   </div>
-     );
+   handleRoomSelect(room){
+     this.props.setActiveRoom(room);
    }
- }
+
+
+
+
+   render () {
+
+     return (
+       <div className="room-list">
+           <h1>{this.props.activeRoom ? this.props.activeRoom.name : 'Select a Chat Room'}</h1>
+           <ul>
+           {this.state.rooms.map( room =>
+             <li key={room.key}>
+             <button className="create-room" onClick={(e) => this.handleRoomSelect(room,e)}>{room.name}</button>
+             </li>
+
+           )}
+           </ul>
+
+           <input type="text" placeholder="Enter new room name..." value={this.state.newRoom} onChange={this.handleUserInput}/>
+           <button onClick={this.createRoom}>Create New Room</button>
+         </div>
+       );
+     }
+   }
+
 
 export default RoomList;
